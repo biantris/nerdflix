4cc36c0e5a8436302340d35e3b872e1115ec05db
BeatrizOliveira250 <65451957+BeatrizOliveira250@users.noreply.github.com>
2020-07-30 10:00:20 -0300

    implementação de rotas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -8,11 +9,11 @@ import Button from '../Button';
 function Menu(){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Logo da beaflix" />
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" href="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
