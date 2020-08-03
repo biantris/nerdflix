9b8307409acc7ec4df83fca2da04a86f2a22233f
BeatrizOliveira250 <65451957+BeatrizOliveira250@users.noreply.github.com>
2020-08-03 16:44:17 -0300

    update

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -12,9 +12,9 @@ function CadastroVideo() {
   const [categorias, setCategorias] = useState([]);
   const categoryTitles = categorias.map(({ titulo }) => titulo);
   const { handleChange, values } = useForm({
-    titulo: 'Video padrão',
-    url: 'https://www.youtube.com/watch?v=jOAU81jdi-c',
-    categoria: 'Front End',
+    titulo: '',
+    url: '',
+    categoria: '',
   });
 
   useEffect(() => {
