42d5ccb6fdbfe873d799e55aef142a5847384cbb
BeatrizOliveira250 <65451957+BeatrizOliveira250@users.noreply.github.com>
2020-08-04 19:46:56 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video';
 import CadastroCategoria from './pages/cadastro/Categoria';
+import PageDefault from './components/PageDefault';
 
-// Desafio master blaster na descrição
-const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
-      <Route path="/cadastro/categoria" component={CadastroCategoria} />
-      <Route component={Pagina404} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route component={() => (
+        <PageDefault>
+          <div class="tenor-gif-embed" data-postid="10902527" data-share-method="host" data-width="100%" data-aspect-ratio="0.8508771929824561"><a href="https://tenor.com/view/no-results-404-gif-10902527">No Results GIF</a> from <a href="https://tenor.com/search/no-gifs">No GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+          <div>
+            <h1>Página 404</h1>
+            <span>
+              Acho que você se perdeu no caminho.
+              <br />
+              Clique no título acima e volte para a página inicial.
+            </span>
+          </div>
+        </PageDefault>
+      )}
+      />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
